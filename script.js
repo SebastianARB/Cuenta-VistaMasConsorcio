3eb5054ab09c66a57cb67f62e3821d0b29e0e1f2
Sebastian Arbona <45405577+SebastianARB@users.noreply.github.com>
2023-09-15 18:34:30 -0300

    webScraping
    se agrego webscraping que obtiene el valor de la uf actual y se muestra en el proyecto principal

--- script.js
+++ script.js
@@ -1,40 +1,48 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const saldoInicial = document.getElementById('saldoInicial');
-    const btnCalcular = document.getElementById('btnCalcular');
-    const ganacia = document.getElementById('ganancia');
-    const valorTotal = document.getElementById('valorTotal');
-    const porcentajeMensual = document.getElementById('porcentajeMensual');
-    const porcentajeAnual = document.getElementById('porcentajeAnual');
-    const gananciaAnual = document.getElementById('gananciaAnual');
-    const sinComision = document.getElementById('sinComision');
+    // Constantes
+    const INTERES_ANUAL = 0.10; // 10%
+    const COMISION_TARJETA = 3300;
 
-    btnCalcular.addEventListener('click', () => {
-        const inicial = parseFloat(saldoInicial.value);
-        const interesAnual = 10 / 100;
-        const interesMensual = (interesAnual / 12); 
-        const comisionTarjeta = 3300;
+    // Elementos del DOM
+    const saldoInicialInput = document.getElementById('saldoInicial');
+    const btnCalcular = document.getElementById('btnCalcular');
+    const gananciaElement = document.getElementById('ganancia');
+    const valorTotalElement = document.getElementById('valorTotal');
+    const porcentajeMensualElement = document.getElementById('porcentajeMensual');
+    const porcentajeAnualElement = document.getElementById('porcentajeAnual');
+    const gananciaAnualElement = document.getElementById('gananciaAnual');
+    const sinComisionElement = document.getElementById('sinComision');
+    const montoAnualSinDescontarElement = document.getElementById('montoAnualSinDescontar');
+
+    // Función para calcular los valores y actualizar el DOM
+    function calcularYActualizar() {
+        const inicial = parseFloat(saldoInicialInput.value);
 
         if (!isNaN(inicial)) {
-            const gananciaGenerada = ((inicial * interesMensual) - comisionTarjeta).toFixed(2);
+            const interesMensual = INTERES_ANUAL / 12;
+            const gananciaGenerada = (inicial * interesMensual - COMISION_TARJETA).toFixed(2);
             const montoTotal = (inicial + parseFloat(gananciaGenerada)).toFixed(2);
             const porcentajeGananciaMensual = (((montoTotal - inicial) / inicial) * 100).toFixed(2);
-            const porcentajeGananciaAnual = (porcentajeGananciaMensual * 12) .toFixed(2);
-            const montoTotalAnual = (inicial + parseFloat(gananciaGenerada * 12 )).toFixed(2);
-            const sinComisionTarjeta = (parseFloat(montoTotal) + comisionTarjeta).toFixed(2);
+            const porcentajeGananciaAnual = (porcentajeGananciaMensual * 12).toFixed(2);
+            const montoTotalAnual = (inicial + parseFloat(gananciaGenerada * 12)).toFixed(2);
+            const sinComisionTarjeta = (parseFloat(montoTotal) + COMISION_TARJETA).toFixed(2);
+            const montoTotalSinDescontar = (inicial + parseFloat(gananciaGenerada) * 12 + COMISION_TARJETA * 12).toFixed(2);
 
-
-            // Formatear números con puntos como separadores de miles
             const formato = {
                 minimumFractionDigits: 2,
                 maximumFractionDigits: 2
             };
 
-            ganacia.textContent = parseFloat(gananciaGenerada).toLocaleString('es-ES', formato);
-            valorTotal.textContent = parseFloat(montoTotal).toLocaleString('es-ES', formato);
-            porcentajeMensual.textContent = parseFloat(porcentajeGananciaMensual).toLocaleString('es-ES', formato) + "%";
-            porcentajeAnual.textContent = parseFloat(porcentajeGananciaAnual).toLocaleString('es-ES', formato) + "%";
-            gananciaAnual.textContent = parseFloat(montoTotalAnual).toLocaleString('es-ES', formato) ;
-            sinComision.textContent = parseFloat(sinComisionTarjeta).toLocaleString('es-ES', formato);
+            gananciaElement.textContent = parseFloat(gananciaGenerada).toLocaleString('es-ES', formato);
+            valorTotalElement.textContent = parseFloat(montoTotal).toLocaleString('es-ES', formato);
+            porcentajeMensualElement.textContent = parseFloat(porcentajeGananciaMensual).toLocaleString('es-ES', formato) + "%";
+            porcentajeAnualElement.textContent = parseFloat(porcentajeGananciaAnual).toLocaleString('es-ES', formato) + "%";
+            gananciaAnualElement.textContent = parseFloat(montoTotalAnual).toLocaleString('es-ES', formato);
+            sinComisionElement.textContent = parseFloat(sinComisionTarjeta).toLocaleString('es-ES', formato);
+            montoAnualSinDescontarElement.textContent = parseFloat(montoTotalSinDescontar).toLocaleString('es-ES', formato);
         }
-    });
+    }
+
+    // Evento clic en el botón Calcular
+    btnCalcular.addEventListener('click', calcularYActualizar);
 });
--- uf.js
+++ uf.js
@@ -1,21 +1,51 @@
-import puppeteer from "puppeteer";
+const puppeteer = require("puppeteer");
+const fs = require("fs").promises; // Importa el módulo fs para trabajar con archivos con promesas
 
-async function precioUF(){
+(async () => {
+  try {
+    // Iniciar el navegador
     const browser = await puppeteer.launch({
-        headless: false,
-        // slowMode: 200
-    })
-    const page = await browser.newPage()
+      headless: false, // Muestra el navegador en pantalla
+      defaultViewport: null, // Usa la configuración de vista predeterminada
+    });
 
-    await page.goto('https://valoruf.cl/')
-    const result = await page.evaluate(() => {
-        const uf = document.querySelector("body > div > div.container > div:nth-child(1) > div > span.vpr").innerText
-        return{
-            uf: uf
-        }
-    })
-    console.log(result)
+    // Abrir una nueva página
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1920, height: 1080 }); // Puedes ajustar el ancho y alto según tus necesidades
+    await page.goto("https://www.ufaldia.cl/");
 
-    await browser.close()
-}
-precioUF();
+    // Recopilar datos
+    const precioUf = await page.evaluate(() => {
+      const items = document.querySelectorAll('[id="vlr-dolar"]');
+      const arr = [];
+
+      for (let item of items) {
+        const uf = {};
+        uf.name = item.querySelector('[id="valor_uf"]').innerText;
+
+        arr.push(uf);
+      }
+      return arr;
+    });
+
+    // Cerrar el navegador
+    await browser.close();
+
+    // Guardar los datos en un archivo JSON
+    const jsonData = JSON.stringify(precioUf, null, 2); // Formatea con sangría
+    await fs.writeFile("precioUf.json", jsonData);
+    console.log("Datos guardados en precioUf.json");
+
+    // Cargar los datos JSON en el HTML
+    const response = await fs.readFile("precioUf.json", "utf8");
+    const data = JSON.parse(response);
+
+    const valorUf = document.getElementById('getPrecioUF');
+    // Suponemos que el valor de la UF se encuentra en el primer objeto del JSON
+    const valorUF = parseFloat(data[0].name.replace(',', '.')).toFixed(3);
+    valorUf.textContent = valorUF; // Actualizamos el contenido del elemento con el valor de la UF
+
+  } catch (error) {
+    console.error("Error:", error);
+  }
+})();
