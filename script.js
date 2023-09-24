9c1247849cff08ecb8d859c934964da2fc235434
Sebastian Arbona <45405577+SebastianARB@users.noreply.github.com>
2023-09-24 00:32:44 -0300

    CalculadoraSantander
    se agrega otra calculadora que brinda el información de las ganancias de la cuentan: Mas Lucas del Banco Santander.

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Constantes
-    const INTERES_ANUAL = 0.10; // 10%
-    const COMISION_TARJETA = 3300;
+    const interes_anual = 0.10; // 10%
+    const interes_anual2 = 0.03; // 3% para Santander
+    const comision_tarjeta = 3300;
 
-    // Elementos del DOM
+    // Elementos del DOM consorcio
     const saldoInicialInput = document.getElementById('saldoInicial');
     const btnCalcular = document.getElementById('btnCalcular');
     const gananciaElement = document.getElementById('ganancia');
@@ -13,20 +14,31 @@ document.addEventListener('DOMContentLoaded', () => {
     const gananciaAnualElement = document.getElementById('gananciaAnual');
     const sinComisionElement = document.getElementById('sinComision');
     const montoAnualSinDescontarElement = document.getElementById('montoAnualSinDescontar');
+    
+    // Elementos del DOM santander
+    const saldoInicialInput2 = document.getElementById('saldoInicial2');
+    const btnCalcular2 = document.getElementById('btnCalcular2');
+    const gananciaElement2 = document.getElementById('ganancia2');
+    const valorTotalElement2 = document.getElementById('valorTotal2');
+    const porcentajeMensualElement2 = document.getElementById('porcentajeMensual2');
+    const porcentajeAnualElement2 = document.getElementById('porcentajeAnual2');
+    const gananciaAnualElement2 = document.getElementById('gananciaAnual2');
+    const sinComisionElement2 = document.getElementById('sinComision2');
+    const montoAnualSinDescontarElement2 = document.getElementById('montoAnualSinDescontar2');
 
-    // Función para calcular los valores y actualizar el DOM
+    // Función para calcular los valores y actualizar el DOM para el consorcio
     function calcularYActualizar() {
         const inicial = parseFloat(saldoInicialInput.value);
 
         if (!isNaN(inicial)) {
-            const interesMensual = INTERES_ANUAL / 12;
-            const gananciaGenerada = (inicial * interesMensual - COMISION_TARJETA).toFixed(2);
+            const interesMensual = interes_anual / 12;
+            const gananciaGenerada = (inicial * interesMensual - comision_tarjeta).toFixed(2);
             const montoTotal = (inicial + parseFloat(gananciaGenerada)).toFixed(2);
             const porcentajeGananciaMensual = (((montoTotal - inicial) / inicial) * 100).toFixed(2);
             const porcentajeGananciaAnual = (porcentajeGananciaMensual * 12).toFixed(2);
             const montoTotalAnual = (inicial + parseFloat(gananciaGenerada * 12)).toFixed(2);
-            const sinComisionTarjeta = (parseFloat(montoTotal) + COMISION_TARJETA).toFixed(2);
-            const montoTotalSinDescontar = (inicial + parseFloat(gananciaGenerada) * 12 + COMISION_TARJETA * 12).toFixed(2);
+            const sinComisionTarjeta = (parseFloat(montoTotal) + comision_tarjeta).toFixed(2);
+            const montoTotalSinDescontar = (inicial + parseFloat(gananciaGenerada) * 12 + comision_tarjeta * 12).toFixed(2);
 
             const formato = {
                 minimumFractionDigits: 2,
@@ -43,6 +55,35 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    // Evento clic en el botón Calcular
+    // Función para calcular los valores y actualizar el DOM para Santander
+    function calcularYActualizar2() {
+        const inicial = parseFloat(saldoInicialInput2.value);
+
+        if (!isNaN(inicial)) {
+            const interesMensual2 = interes_anual2 / 12;
+            const gananciaGenerada = (inicial * interesMensual2).toFixed(2);
+            const montoTotal = (inicial + parseFloat(gananciaGenerada)).toFixed(2);
+            const porcentajeGananciaMensual = (((montoTotal - inicial) / inicial) * 100).toFixed(2);
+            const porcentajeGananciaAnual = (porcentajeGananciaMensual * 12).toFixed(2);
+            const montoTotalAnual = (inicial + parseFloat(gananciaGenerada * 12)).toFixed(2);
+            const formato = {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2
+            };
+
+            gananciaElement2.textContent = parseFloat(gananciaGenerada).toLocaleString('es-ES', formato);
+            valorTotalElement2.textContent = parseFloat(montoTotal).toLocaleString('es-ES', formato);
+            porcentajeMensualElement2.textContent = parseFloat(porcentajeGananciaMensual).toLocaleString('es-ES', formato) + "%";
+            porcentajeAnualElement2.textContent = parseFloat(porcentajeGananciaAnual).toLocaleString('es-ES', formato) + "%";
+            gananciaAnualElement2.textContent = parseFloat(montoTotalAnual).toLocaleString('es-ES', formato);
+            sinComisionElement2.textContent = parseFloat(sinComisionTarjeta).toLocaleString('es-ES', formato);
+            montoAnualSinDescontarElement2.textContent = parseFloat(montoTotalSinDescontar).toLocaleString('es-ES', formato);
+        }
+    }
+
+    // Evento clic en el botón Calcular para el consorcio
     btnCalcular.addEventListener('click', calcularYActualizar);
+
+    // Evento clic en el botón Calcular para Santander
+    btnCalcular2.addEventListener('click', calcularYActualizar2);
 });
